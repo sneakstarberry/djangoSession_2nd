19e155543dc988ba51a8e4a787bceaa9109a8c43
sneakstarberry <gihong2012@likelion.org>
2020-05-31 16:06:13 +0900

    완성

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length= 100)
     body = models.CharField(max_length=1000)
+    img = models.ImageField(upload_to = "blog/image")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
